19a5c7a7fc03a272a75a9c2f403761c3354bf3b2
SrinivasNik <srinivas.d@diginik.in>
2026-03-31 16:15:52 +0530

    ok

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World! ok');
 });
 
 const PORT = process.env.PORT || 8080;
