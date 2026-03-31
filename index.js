a5fba681f2ed2158a2e58ec2f4ece8f2b96dc68e
SrinivasNik <srinivas.d@diginik.in>
2026-03-31 16:40:01 +0530

    update fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello srinivas do fast!');
+    res.send('Hello srinivas do fast fast!');
 });
 
 const PORT = process.env.PORT || 8080;
